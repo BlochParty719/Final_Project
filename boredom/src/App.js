753230e3f70830a80b4b47a922f07ab33ee0adf6
Daniel <danieljbloch95@gmail.com>
2019-12-24 13:38:25 -0500

    Pushing

--- boredom/src/App.js
+++ boredom/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-//import Activities from './src/Components/Activities.js'
+import Activities from './Components/Activities.js'
 //import Activity from './Components/Activity.js'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      baseurl: 'http://www.boredapi.com',
-      // apikey: 'apikey=' + '',
-      query: '&t=',
       activity: '',
       type: '',
       participants: ''
@@ -17,51 +14,61 @@ class App extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this)
   }
   handleChange(event) {
-  this.setState({[event.target.id] : event.target.value})
-}
-handleSubmit(event) {
-  event.preventDefault()
-  this.setState({
-    searchURL: this.state.baseURL + this.state.query + this.state.activity
-}, () => {
-  fetch(this.searchURL)
-  .then(response => {
-    return response.json()
-  })
-  .then(json => this.setState({
-    activity: '',
-    type: '',
-    participants: '',
-  }),
-  error => console.log(error)
+    this.setState({[event.target.id] : event.target.value})
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    this.setState({
+      searchURL: this.state.baseURL + this.state.query + this.state.activity
+    }, () => {
+      fetch(this.searchURL)
+      .then(response => {
+        return response.json()
+      })
+      .then(json => this.setState({
+        activity: '',
+        type: '',
+        participants: '',
+      }),
+      error => console.log(error)
     )
   })
 }
-  render () {
-    return(
-      <div>
+render () {
+  return(
+    <div>
       <header>
-      <h1>Home</h1>
-
+        <h1>Home</h1>
       </header>
       <body>
-      <div className = 'topbar-row'>
+      <div className='table'>
+      <table>
       <h1>The Board of Boredom</h1>
       <h2>You should: {this.state.activity}</h2>
       <h2>Type: {this.state.type}</h2>
       <h2>Number of people required: {this.state.participants}</h2>
-      <button>Generate</button>
+      <button>Random</button>
+      </table>
       </div>
-      <div class='side-nav'>
-
-      </div>
+    <div class='side-nav'>
+    <h3>Categories</h3>
+      <ul>
+        <li>BusyWork</li>
+        <li>Cooking</li>
+        <li>DIY</li>
+        <li>Educational</li>
+        <li>Music</li>
+        <li>Recreational</li>
+        <li>Social</li>
+        </ul>
+        </div>
       </body>
       <footer>
-      <div class='socials'>
+        <div class='socials'>
 
-      </div>
+        </div>
       </footer>
-      </div>
+    </div>
     )
   }
 }
--- boredom/src/Components/Activities.js
+++ boredom/src/Components/Activities.js
@@ -4,8 +4,14 @@ import Activity from './Activity.js'
 class Activities extends React.Component {
   render () {
     return (
-      <div className="Activities">
-      
+      <div className='Activities'>
+      <table>
+      <h1>The Board of Boredom</h1>
+      <h2>You should: {this.state.activity}</h2>
+      <h2>Type: {this.state.type}</h2>
+      <h2>Number of people required: {this.state.participants}</h2>
+      <button>Random</button>
+      </table>
       </div>
     )
   }
