444aad2c4b86b608b9cbfde6eb002bf583f11f40
Daniel <danieljbloch95@gmail.com>
2019-12-27 13:03:04 -0500

    Got API Working

--- boredom/src/App.js
+++ boredom/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-//import Activities from './Components/Activities'
 
 class App extends React.Component {
   constructor(props) {
@@ -10,32 +9,23 @@ class App extends React.Component {
       activity: '',
       type: '',
       participants: '',
-      searchURL: ''
     }
-    // this.getActivity = this.getActivity.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.getActivity = this.getActivity.bind(this)
   }
-  handleChange (event) {
-    this.setState({[event.target.id] : event.target.value})
-  }
-  handleSubmit (event) {
-    event.preventDefault()
-    this.setState({
-      searchURL: this.state.baseURL + this.state.query + this.state.activity + this.state.type + this.state.participants
-    }, () => {
-  //getActivity(){
+  getActivity(){
     fetch('http://www.boredapi.com/api/activity')
     .then(response => {
     return response.json()
     }).then(json => this.setState({
       activity: json,
-      activity: '',
-      type: '',
-      participants: ''
-    }), error => console.log(error))
-  })
-}
+      activity: json.activity,
+      type: json.type,
+      participants: json.participants
+    }),
+    error => console.log(error)
+    )
+  }
+
 render () {
   return(
     <>
@@ -43,30 +33,27 @@ render () {
       <h1>Home</h1>
       </header>
     <body>
-      <form>
+    <div className='container'>
       <h1>The Board of Boredom</h1>
-      <div className='container'>
-      <form onSubmit={this.handleSubmit}>
-      <label htmlFor='activity'> You Should: {this.state.activity}</label>
-
+    <div className='info'>
+      <h2> You Should: {this.state.activity}</h2>
       <h2>Type: {this.state.type}</h2>
       <h2>Number of people required: {this.state.participants}</h2>
-    </form>
-    <button onClick={this.getActivity}>Random</button>
+    <button onClick={this.getActivity} type='button'>Random</button>
+    </div>
     </div>
     <div class='side-nav'>
       <h3>Categories</h3>
         <ul>
-          <li>BusyWork</li>
-          <li>Cooking</li>
-          <li>DIY</li>
-          <li>Educational</li>
-          <li>Music</li>
-          <li>Recreational</li>
-          <li>Social</li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
         </ul>
     </div>
-    </form>
     </body>
     <footer>
       <div class='socials'>
